3e9f3d37b2b60b9335429727c2d87ab211618845
Ellery <allotory@msn.com>
2015-06-26 16:54:06 +0800

    Init:add project

---
+++ Budget/src/com/test/Main.java
@@ -0,0 +1,22 @@
+package com.test;
+
+import javax.swing.UIManager;
+
+import org.jb2011.lnf.beautyeye.BeautyEyeLNFHelper;
+
+import com.test.ui.Budget;
+
+public class Main {
+	public static void main(String[] args) {
+		try{
+			BeautyEyeLNFHelper.frameBorderStyle = BeautyEyeLNFHelper.FrameBorderStyle.translucencyAppleLike;
+	        BeautyEyeLNFHelper.launchBeautyEyeLNF();
+	        UIManager.put("RootPane.setupButtonVisible", false);
+	    }catch(Exception e){
+	    	e.printStackTrace();
+	    }
+		Budget budget = new Budget();
+		budget.init();
+		budget.setVisible(true);
+	}
+}
