675da229308b01c486265d0f086ac93277011ae1
Ellery <allotory@msn.com>
2015-06-27 15:47:16 +0800

    Add:add card layout and index data stat

--- Budget/src/com/test/ui/Budget.java
+++ Budget/src/com/test/ui/Budget.java
@@ -1,12 +1,16 @@
 package com.test.ui;
 
 import java.awt.BorderLayout;
+import java.awt.CardLayout;
+import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.GridBagLayout;
 import java.awt.GridLayout;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
@@ -114,7 +118,40 @@ public class Budget extends JFrame{
     private JPanel centerPanel = null;
     //中心Panel - title border
     private TitledBorder centerTBorder = null;
-
+    //设置中心卡片布局
+  	private CardLayout card = null;
+    
+    //首页card panel
+    private JPanel indexPanel = null;
+    //首页数据统计Panel
+    private JPanel dataStatPanel = null;
+    //首页数据统计title border
+    private TitledBorder dataStatTBorder = null;
+    
+    //首页数据统计 - 本周收入/支出/盈余
+    private JLabel iweekLabel = null;
+    private JLabel eweekLabel = null;
+    private JLabel weekSurplusLabel = null;
+    //首页数据统计 - 本月收入/支出/盈余
+    private JLabel imonthLabel = null;
+    private JLabel emonthLabel = null;
+    private JLabel monthSurplusLabel = null;
+    //首页数据统计 - 本季度收入/支出/盈余
+    private JLabel iquarterLabel = null;
+    private JLabel equarterLabel = null;
+    private JLabel quarterSurplusLabel = null;
+    //首页数据统计 - 本年收入/支出/盈余
+    private JLabel iyearLabel = null;
+    private JLabel eyearLabel = null;
+    private JLabel yearSurplusLabel = null;
+    //首页数据统计 - 全部收入/支出/盈余
+    private JLabel iallLabel = null;
+    private JLabel eallLabel = null;
+    private JLabel allSurplusLabel = null;
+    
+    //新增收支 Panel
+    private JPanel newSchePanel = null;
+    
 	//获取屏幕高度宽度
 	private ScreenSizeUtils screenSizeUtils = null;
 	
@@ -124,6 +161,7 @@ public class Budget extends JFrame{
 		this.setLayout(null);
 		this.setScreen();
 		this.setMenu();
+		this.setResizable(false);
 		this.setBounds((screenWidth - windowWidth)/2, 
 				(screenHeight - windowHeight)/2, windowWidth, windowHeight);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -143,6 +181,7 @@ public class Budget extends JFrame{
 	//设置菜单
 	public void setMenu() {
 		menuBar = new JMenuBar();
+		
 		//文件菜单
 		fileMenu = new JMenu("文件");
 		newMItem = new JMenuItem("新增收支");
@@ -152,6 +191,14 @@ public class Budget extends JFrame{
 		fileMenu.add(newMItem);
 		fileMenu.add(addTypeMItem);
 		fileMenu.add(exitMItem);
+		newMItem.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mousePressed(MouseEvent e) {
+				//显示新增开支
+				card.show(centerPanel, "newSchedule");
+			}
+		});
+		
 		//收支明细菜单
 		scheduleMenu = new JMenu("收支明细");
 		scheduleMItem = new JMenuItem("收支明细");
@@ -160,6 +207,7 @@ public class Budget extends JFrame{
 		scheduleMenu.add(scheduleMItem);
 		scheduleMenu.add(scheStatMItem);
 		scheduleMenu.add(graphMItem);
+		
 		//支出分析菜单
 		expensesMenu = new JMenu("支出分析");
 		edayMItem = new JMenuItem("日段分析");
@@ -168,6 +216,7 @@ public class Budget extends JFrame{
 		expensesMenu.add(edayMItem);
 		expensesMenu.add(emonthMItem);
 		expensesMenu.add(eyearMItem);
+		
 		//收入分析菜单
 		incomeMenu = new JMenu("支出分析");
 		idayMItem = new JMenuItem("日段分析");
@@ -176,6 +225,7 @@ public class Budget extends JFrame{
 		incomeMenu.add(idayMItem);
 		incomeMenu.add(imonthMItem);
 		incomeMenu.add(iyearMItem);
+		
 		//帮助菜单
 		helpMenu = new JMenu("帮助");
 		aboutMItem = new JMenuItem("关于");
@@ -241,8 +291,88 @@ public class Budget extends JFrame{
 	//设置中心内容
 	public void setContent() {
 		centerPanel = new JPanel();
-		centerTBorder = new TitledBorder("首页");
+		centerTBorder = new TitledBorder("");
 		centerPanel.setBorder(centerTBorder);
+		
+		//设置中心卡片布局
+		card = new CardLayout();
+		centerPanel.setLayout(card);
+		
+		//设置首页
+		this.setIndex();
+		//设置新增开支
+		this.setNewSche();
+		
+		//显示首页
+		card.show(centerPanel, "index");
+		
 		mainPanel.add(BorderLayout.CENTER, centerPanel);
+	}
+	
+	public void setIndex() {
+		//首页卡片Panel
+		indexPanel = new JPanel(new BorderLayout());
+		indexPanel.setBackground(Color.PINK);
+		
+		//数据统计
+		dataStatPanel = new JPanel(new GridLayout(5, 3));
+		dataStatTBorder = new TitledBorder("数据统计");
+		dataStatPanel.setBorder(dataStatTBorder);
+		
+		//数据统计详细信息
+		//本周
+		iweekLabel = new JLabel("本周收入：0");
+		eweekLabel = new JLabel("本周支出：0");
+		weekSurplusLabel = new JLabel("本周盈余：0");
+		//本月
+		imonthLabel = new JLabel("本月收入：0");
+		emonthLabel = new JLabel("本月支出：0");
+		monthSurplusLabel = new JLabel("本月盈余：0");
+		//本季度
+		iquarterLabel = new JLabel("本季度收入：0");
+		equarterLabel = new JLabel("本季度支出：0");
+		quarterSurplusLabel = new JLabel("本季度盈余：0");
+		//本年
+		iyearLabel = new JLabel("本年收入：0");
+		eyearLabel = new JLabel("本年支出：0");
+		yearSurplusLabel = new JLabel("本年盈余：0");
+		//全部
+		iallLabel = new JLabel("总收入：0");
+		eallLabel = new JLabel("总支出：0");
+		allSurplusLabel = new JLabel("总盈余：0");
+		
+		//添加周
+		dataStatPanel.add(iweekLabel);
+		dataStatPanel.add(eweekLabel);
+		dataStatPanel.add(weekSurplusLabel);
+		//添加月
+		dataStatPanel.add(imonthLabel);
+		dataStatPanel.add(emonthLabel);
+		dataStatPanel.add(monthSurplusLabel);
+		//添加季度
+		dataStatPanel.add(iquarterLabel);
+		dataStatPanel.add(equarterLabel);
+		dataStatPanel.add(quarterSurplusLabel);
+		//添加年度
+		dataStatPanel.add(iyearLabel);
+		dataStatPanel.add(eyearLabel);
+		dataStatPanel.add(yearSurplusLabel);
+		//添加全部
+		dataStatPanel.add(iallLabel);
+		dataStatPanel.add(eallLabel);
+		dataStatPanel.add(allSurplusLabel);
+		
+		indexPanel.add(BorderLayout.NORTH, dataStatPanel);
+		
+		//添加到卡片
+		centerPanel.add("index", indexPanel);
+	}
+	
+	public void setNewSche() {
+		//新增收支
+		newSchePanel = new JPanel();
+		
+		//添加到卡片
+		centerPanel.add("newSchedule", newSchePanel);
 	}
 }
