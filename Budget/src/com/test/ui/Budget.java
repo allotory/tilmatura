4c2e26bfa384b017c8f34617f37e936583a79844
Ellery <allotory@msn.com>
2015-06-27 10:15:41 +0800

    Add:add sidebar ui

--- Budget/src/com/test/ui/Budget.java
+++ Budget/src/com/test/ui/Budget.java
@@ -1,12 +1,17 @@
 package com.test.ui;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JPanel;
+import javax.swing.border.TitledBorder;
 
 import com.test.utils.ScreenSizeUtils;
 
@@ -69,6 +74,47 @@ public class Budget extends JFrame{
     //主窗口Panel
     private JPanel mainPanel = null;
 
+    //左侧导航Panel
+    private JPanel westPanel = null;
+    
+    //左侧导航 - 收支明细Panel
+    private JPanel schedulePanel = null;
+    //左侧导航 - 收支明细title border
+    private TitledBorder scheduleTBorder = null;
+    //左侧导航 - 收支明细按钮
+    private JButton scheduleBtn = null;
+    //左侧导航 - 收支统计按钮
+    private JButton scheStatBtn = null;
+    //左侧导航 - 收支图表按钮
+    private JButton graphBtn = null;
+    
+    //左侧导航 - 支出分析Panel
+    private JPanel expAnalysisPanel = null;
+    //左侧导航 - 支出分析title border
+    private TitledBorder expAnaylysisTBorder = null;
+    //左侧导航 - 日段分析
+    private JButton edayBtn = null;
+    //左侧导航 - 月度分析
+    private JButton emonthBtn = null;
+    //左侧导航 - 年度分析
+    private JButton eyearBtn = null;
+    
+    //左侧导航 - 收入分析Panel
+    private JPanel inAnalysisPanel = null;
+    //左侧导航 - 收入分析title border
+    private TitledBorder inAnalysisTBorder = null;
+    //左侧导航 - 日段分析
+    private JButton idayBtn = null;
+    //左侧导航 - 月度分析
+    private JButton imonthBtn = null;
+    //左侧导航 - 年度分析
+    private JButton iyearBtn = null;
+    
+    //中心Panel
+    private JPanel centerPanel = null;
+    //中心Panel - title border
+    private TitledBorder centerTBorder = null;
+
 	//获取屏幕高度宽度
 	private ScreenSizeUtils screenSizeUtils = null;
 	
@@ -82,6 +128,8 @@ public class Budget extends JFrame{
 				(screenHeight - windowHeight)/2, windowWidth, windowHeight);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		mainPanel = new JPanel(new BorderLayout());
+		this.setSideBar();
+		this.setContent();
 		this.setContentPane(mainPanel);
 	}
 	
@@ -140,5 +188,61 @@ public class Budget extends JFrame{
 		menuBar.add(helpMenu);
 		
 		this.setJMenuBar(menuBar);
+	}
+	
+	//设置左侧导航
+	public void setSideBar() {
+		westPanel = new JPanel(new GridLayout(3, 1));
+		westPanel.setPreferredSize(new Dimension(130, 0));
+		
+		//收支明细
+		schedulePanel = new JPanel(new GridLayout(3, 1));
+		scheduleTBorder = new TitledBorder("收支明细");
+		schedulePanel.setBorder(scheduleTBorder);
+		scheduleBtn = new JButton("收支明细");
+		scheStatBtn = new JButton("收支统计");
+		graphBtn = new JButton("收支图表");
+		
+		schedulePanel.add(scheduleBtn);
+		schedulePanel.add(scheStatBtn);
+		schedulePanel.add(graphBtn);
+		
+		//支出分析
+		expAnalysisPanel = new JPanel(new GridLayout(3, 1));
+		expAnaylysisTBorder = new TitledBorder("支出分析");
+		expAnalysisPanel.setBorder(expAnaylysisTBorder);
+		edayBtn = new JButton("日段分析");
+		emonthBtn = new JButton("月度分析");
+		eyearBtn = new JButton("年度分析");
+		
+		expAnalysisPanel.add(edayBtn);
+		expAnalysisPanel.add(emonthBtn);
+		expAnalysisPanel.add(eyearBtn);
+		
+		//收入分析
+		inAnalysisPanel = new JPanel(new GridLayout(3, 1));
+		inAnalysisTBorder = new TitledBorder("收入分析");
+		inAnalysisPanel.setBorder(inAnalysisTBorder);
+		idayBtn = new JButton("日段分析");
+		imonthBtn = new JButton("月度分析");
+		iyearBtn = new JButton("年度分析");
+		
+		inAnalysisPanel.add(idayBtn);
+		inAnalysisPanel.add(imonthBtn);
+		inAnalysisPanel.add(iyearBtn);
+		
+		westPanel.add(schedulePanel);
+		westPanel.add(expAnalysisPanel);
+		westPanel.add(inAnalysisPanel);
+		
+		mainPanel.add(BorderLayout.WEST, westPanel);
+	}
+	
+	//设置中心内容
+	public void setContent() {
+		centerPanel = new JPanel();
+		centerTBorder = new TitledBorder("首页");
+		centerPanel.setBorder(centerTBorder);
+		mainPanel.add(BorderLayout.CENTER, centerPanel);
 	}
 }
