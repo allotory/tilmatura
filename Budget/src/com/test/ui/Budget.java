df6b83f8f7b76425dda2260103c4bff0154288e9
Ellery <allotory@msn.com>
2015-06-28 15:52:22 +0800

    Add: add sche details frame

--- Budget/src/com/test/ui/Budget.java
+++ Budget/src/com/test/ui/Budget.java
@@ -4,6 +4,7 @@ import java.awt.BorderLayout;
 import java.awt.CardLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
@@ -17,6 +18,8 @@ import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 import javax.swing.JTextField;
 import javax.swing.border.TitledBorder;
 
@@ -178,6 +181,26 @@ public class Budget extends JFrame{
     //新增收支 - 提交
     private JButton addScheBtn = null;
     
+    //收支明细 Panel
+    private JPanel scheDetailPanel = null;
+    //收支明细 title border
+    private TitledBorder scheDetailTBorder = null;
+    //收支明细 - 标题Panel
+    private JPanel scheDeTitlePanel = null;
+    //收支明细 - 起始日期
+    private JLabel startDateLabel = null;
+    private JTextField startDate = null;
+    //收支明细 - 结束日期
+    private JLabel endDateLabel = null;
+    private JTextField endDate = null;
+    //收支明细 - 类别
+    private JLabel scheTypeLabel = null;
+    private JComboBox<String> scheTypeCbox = null;
+    //收支明细 - 查询
+    private JButton queryBtn = null;
+    //收支明细 - 表格
+    private JTable scheTable = null;
+    
 	//获取屏幕高度宽度
 	private ScreenSizeUtils screenSizeUtils = null;
 	
@@ -278,6 +301,13 @@ public class Budget extends JFrame{
 		scheduleBtn = new JButton("收支明细");
 		scheStatBtn = new JButton("收支统计");
 		graphBtn = new JButton("收支图表");
+		scheduleBtn.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mousePressed(MouseEvent e) {
+				//显示收支明细
+				card.show(centerPanel, "scheDetail");
+			}
+		});
 		
 		schedulePanel.add(scheduleBtn);
 		schedulePanel.add(scheStatBtn);
@@ -326,6 +356,8 @@ public class Budget extends JFrame{
 		this.setIndex();
 		//设置新增开支
 		this.setNewSche();
+		//设置收支明细
+		this.scheDetails();
 		
 		//显示首页
 		card.show(centerPanel, "index");
@@ -467,5 +499,60 @@ public class Budget extends JFrame{
 		
 		//添加到卡片
 		centerPanel.add("newSchedule", newSchePanel);
+	}
+	
+	public void scheDetails() {
+		scheDetailPanel = new JPanel(new BorderLayout());
+		scheDetailTBorder = new TitledBorder("收支明细");
+		scheDetailPanel.setBorder(scheDetailTBorder);
+
+		//标题Panel
+		scheDeTitlePanel = new JPanel(new FlowLayout());
+		//起始日期
+		startDateLabel = new JLabel("起始日期：");
+		startDate = new JTextField(10);
+		//结束日期
+		endDateLabel = new JLabel("结束日期：");
+		endDate = new JTextField(10);
+		//类别
+		scheTypeLabel = new JLabel("类别：");
+		scheTypeCbox = new JComboBox<String>();
+		scheTypeCbox.addItem("衣");
+		scheTypeCbox.addItem("食");
+		scheTypeCbox.addItem("住");
+		scheTypeCbox.addItem("行");
+		//查询按钮
+		queryBtn = new JButton("查询");
+
+		//添加起始如期
+		scheDeTitlePanel.add(startDateLabel);
+		scheDeTitlePanel.add(startDate);
+		//添加结束如期
+		scheDeTitlePanel.add(endDateLabel);
+		scheDeTitlePanel.add(endDate);
+		//添加类别
+		scheDeTitlePanel.add(scheTypeLabel);
+		scheDeTitlePanel.add(scheTypeCbox);
+		//添加查询按钮
+		scheDeTitlePanel.add(queryBtn);
+		
+		//定义二维数组作为表格数据  
+	    Object[][] tableData = {  
+	        new Object[]{"李清照" , 29 ,29 , "女"},  
+	        new Object[]{"苏格拉底", 29 , 56 , "男"},  
+	        new Object[]{"李白", 29 , 35 , "男"},  
+	        new Object[]{"弄玉", 29 , 18 , "女"},  
+	        new Object[]{"虎头", 29 , 2 , "男"}  
+	    };  
+	    //定义一维数据作为列标题  
+	    Object[] columnTitle = {"日期" , "收入" , "支出", "合计"};  
+		//收支明细表格
+		scheTable = new JTable(tableData, columnTitle);
+		
+		scheDetailPanel.add(BorderLayout.NORTH, scheDeTitlePanel);
+		scheDetailPanel.add(BorderLayout.CENTER, new JScrollPane(scheTable));
+		
+		//添加到卡片
+		centerPanel.add("scheDetail", scheDetailPanel);
 	}
 }
